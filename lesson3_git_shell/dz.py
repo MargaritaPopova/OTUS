e9f3be38643e979698bf57dbe4e87bc9c42c2cfc
Margarita Popova <margarita.kistanova@gmail.com>
2020-08-18 10:30:12 +0300

    Filter function fixed

--- lesson3_git_shell/dz.py
+++ lesson3_git_shell/dz.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from functools import wraps
 from time import time
 
@@ -30,22 +31,26 @@ def prime(n):
 
 @timed
 def filter_list(numbers, value_type):
-    if value_type == 'even':
+    if value_type is Nums.EVENS:
         return list(filter(lambda n: n % 2 == 0, numbers))
-    elif value_type == 'odd':
+    elif value_type is Nums.ODDS:
         return list(filter(lambda n: n % 2 != 0, numbers))
     return list(filter(prime, numbers))
 
 
-FILTER_BY = ['even', 'odd', 'prime']
+class Nums(Enum):
+    EVENS = 1
+    ODDS = 2
+    PRIMES = 3
 
 
 def main():
     print('Power function with default power:', process_list([1, 4, 32, 7, 1000]))
     print('Power function with predefined power:', process_list([1, 4, 32, 7, 1000], 8))
-    print('Filter list by evens:', filter_list([1, 2, 3, 4, 398], FILTER_BY[0]))
-    print('Filter list by odds:', filter_list([1, 2, 3, 4, 398], FILTER_BY[1]))
-    print('Filter list by primes:', filter_list([1, 2, 3, 4, 398], FILTER_BY[2]))
+
+    print('Filter list by evens:', filter_list([1, 2, 3, 4, 398], Nums.EVENS))
+    print('Filter list by odds:', filter_list([1, 2, 3, 4, 398], Nums.ODDS))
+    print('Filter list by primes:', filter_list([1, 2, 3, 4, 398], Nums.PRIMES))
 
 
 if __name__ == "__main__":
