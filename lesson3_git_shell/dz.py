43696b4ba4e57a5d10e083eda2586343062898b7
Margarita Popova <margarita.kistanova@gmail.com>
2020-08-18 10:08:45 +0300

    process_list function fixed

--- lesson3_git_shell/dz.py
+++ lesson3_git_shell/dz.py
@@ -15,14 +15,8 @@ def timed(func):
     return wrapper
 
 
-def raise_to_power(num, power=2):
-    return pow(num, power)
-
-
-def process_list(func, *args, power=None):
-    if power is None:
-        return list(map(func, args))
-    return [func(v, power) for v in args]
+def process_list(data: list, power=2):
+    return [pow(v, power) for v in data]
 
 
 def prime(n):
@@ -47,8 +41,8 @@ FILTER_BY = ['even', 'odd', 'prime']
 
 
 def main():
-    print('Power function with default power:', process_list(raise_to_power, 1, 4, 32, 7, 1000))
-    print('Power function with predefined power:', process_list(raise_to_power, 1, 4, 32, 7, 1000, power=8))
+    print('Power function with default power:', process_list([1, 4, 32, 7, 1000]))
+    print('Power function with predefined power:', process_list([1, 4, 32, 7, 1000], 8))
     print('Filter list by evens:', filter_list([1, 2, 3, 4, 398], FILTER_BY[0]))
     print('Filter list by odds:', filter_list([1, 2, 3, 4, 398], FILTER_BY[1]))
     print('Filter list by primes:', filter_list([1, 2, 3, 4, 398], FILTER_BY[2]))
