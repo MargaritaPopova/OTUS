b2ac294342b3f0965193753912e8b2a29d96813f
Margarita Popova <margarita.kistanova@gmail.com>
2020-10-30 22:48:14 +0300

    Edited main view

--- expences_proj/expences/main/views.py
+++ expences_proj/expences/main/views.py
@@ -8,8 +8,6 @@ class MainView(View):
     template_name = 'main/index.html'
 
     def get(self, request):
-        for i in range(5):
-            t = TestModel.objects.create(name="entry", type=i)
         test_models = TestModel.objects.order_by('id')
 
         context = {'name': 'Expences app', 'test_models': test_models}
