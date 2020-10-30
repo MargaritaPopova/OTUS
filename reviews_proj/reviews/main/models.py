c3bd7ca9af5ee39b0ba6dbaca9fef69e9516d2d7
Margarita Popova <margarita.kistanova@gmail.com>
2020-10-30 13:29:02 +0300

    deleted reviews_proj

--- reviews_proj/reviews/main/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-from django.db import models
-
-
-class Review(models.Model):
-
-    text = models.CharField(max_length=5000),
-    grade = models.IntegerField(default=0),
-    author = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
