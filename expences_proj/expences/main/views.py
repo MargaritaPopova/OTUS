d5df3a0e3bdedf03a288e4a3e5acc154315ae4d1
Margarita Popova <margarita.kistanova@gmail.com>
2020-10-30 22:22:57 +0300

    test model added

--- expences_proj/expences/main/views.py
+++ expences_proj/expences/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.base import View
+from .models import TestModel
 
 
 class MainView(View):
@@ -7,5 +8,9 @@ class MainView(View):
     template_name = 'main/index.html'
 
     def get(self, request):
-        context = {'name': 'Expences app'}
+        for i in range(5):
+            t = TestModel.objects.create(name="entry", type=i)
+        test_models = TestModel.objects.order_by('id')
+
+        context = {'name': 'Expences app', 'test_models': test_models}
         return render(request, self.template_name, context)
