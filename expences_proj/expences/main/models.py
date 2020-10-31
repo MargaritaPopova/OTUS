e208d751ca644ae3cd95c0ea47d605609a42e72c
Margarita Popova <margarita.kistanova@gmail.com>
2020-10-31 13:58:24 +0300

    Added kind field to test model

--- expences_proj/expences/main/models.py
+++ expences_proj/expences/main/models.py
@@ -5,6 +5,7 @@ class TestModel(models.Model):
 
     name = models.CharField(max_length=64, default='')
     type = models.IntegerField(null=False)
+    kind = models.CharField(max_length=64, default='')
 
     def __str__(self):
         return self.name
