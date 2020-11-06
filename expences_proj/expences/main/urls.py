da4b71054fa4b0a772a3ba2ba0054c3ad801d5d7
Margarita Popova <margarita.kistanova@gmail.com>
2020-11-06 22:53:00 +0300

    CRUD flow implemented for currencies

--- expences_proj/expences/main/urls.py
+++ expences_proj/expences/main/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
-from .views import CurrencyMainView, CurrencyDetailView, CurrencyListView
+from .views import \
+    CurrencyMainView, \
+    CurrencyDetailView, \
+    CurrencyListView, \
+    CurrencyCreateView, \
+    CurrencyDeleteView, \
+    CurrencyUpdateView
 
 app_name = 'main'
 
@@ -7,4 +13,8 @@ urlpatterns = [
     path('', CurrencyMainView.as_view(), name='index'),
     path('main/list/', CurrencyListView.as_view(), name='list'),
     path('main/<int:pk>/', CurrencyDetailView.as_view(), name='detail'),
+    path('main/create/', CurrencyCreateView.as_view(), name='create'),
+    path('main/<int:pk>/update', CurrencyUpdateView.as_view(), name='update'),
+    path('main/<int:pk>/delete', CurrencyDeleteView.as_view(), name='delete'),
+
 ]
--- expences_proj/expences/main/views.py
+++ expences_proj/expences/main/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from .models import Currency
 
 
 class CurrencyMainView(View):
 
-    template_name = 'main/index.html'
+    template_name = 'main/currency_index.html'
 
     def get(self, request):
         context = {
@@ -15,7 +17,7 @@ class CurrencyMainView(View):
         return render(request, self.template_name, context)
 
 
-class  CurrencyListView(ListView):
+class CurrencyListView(ListView):
 
     model = Currency
     template_name = 'main/currency_list.html'
@@ -25,4 +27,24 @@ class CurrencyDetailView(DetailView):
 
     model = Currency
     template_name = 'main/currency_detail.html'
+
+
+class CurrencyCreateView(CreateView):
+    model = Currency
+    fields = ['name']
+    success_url = reverse_lazy('main:index')
+
+
+class CurrencyUpdateView(UpdateView):
+    model = Currency
+    fields = ['name']
+    success_url = reverse_lazy('main:index')
+
+
+class CurrencyDeleteView(DeleteView):
+    model = Currency
+    fields = ['name']
+    template_name = 'main/currency_confirm_delete.html'
+    success_url = reverse_lazy('main:index')
+
 
